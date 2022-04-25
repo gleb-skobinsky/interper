7a43b07f08c7b8b8b1aeb7aa0ea8bebd67fed96f
kot-ne-kod-77 <90451272+kot-ne-kod-77@users.noreply.github.com>
2022-04-25 21:13:53 +0300

    update urls

--- interp/urls.py
+++ interp/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls.conf import include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("core.urls", namespace="core")),
+    path(r'favicon.ico',RedirectView.as_view(url='/static/images/favicon.ico')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
